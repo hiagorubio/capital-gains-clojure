1cd7fa710c25fe7f56da2a217292286acacb2654
hiago.rubio <hiagorubio@gmail.com>
2024-06-07 11:28:54 -0300

    Fix review comments

--- src/capital_gains/logic/calculate.clj
+++ src/capital_gains/logic/calculate.clj
@@ -1,79 +1,78 @@
 (ns capital-gains.logic.calculate
-	(:require [capital-gains.logic.helpers :as h]
-																						 [capital-gains.models :as m]
-																						 [schema.core :as s]))
+  (:require [capital-gains.logic.helpers :as h]
+            [capital-gains.models :as m]
+            [schema.core :as s]))
 
-(s/defn have-loss? [unit-cost, actual-weighted-average-price]
-	(< unit-cost actual-weighted-average-price))
+(s/def empty-tax {:tax 0.00})
+(s/def tax 0.2)
+(s/def operation-limit 20000.00)
+
+
+(s/defn operation-has-loss? [unit-cost, actual-weighted-average-price]
+  (< unit-cost actual-weighted-average-price))
 
 (s/defn calculate-loss [acc :- m/Accumulator operation :- m/Operation]
-	(if (h/is-buy-operation? operation)
-		(:loss acc)
+  (if (h/is-buy-operation? operation)
+    (:loss acc)
 
-		(let [
-					actual-weighted-average-price (float (:weighted-average-price acc))
-					unit-cost                     (float (:unit-cost operation))
-					quantity                      (float (:quantity operation))
-					]
-			(if (have-loss? unit-cost actual-weighted-average-price)
-				(* (- unit-cost actual-weighted-average-price) quantity)
-				(+ (:loss acc) (h/calculate-profit acc operation))
-				))))
-
-(s/defn return-empty-tax [] {:tax 0.00})
+    (let [
+          actual-weighted-average-price (float (:weighted-average-price acc))
+          unit-cost                     (float (:unit-cost operation))
+          quantity                      (float (:quantity operation))
+          ]
+      (if (operation-has-loss? unit-cost actual-weighted-average-price)
+        (* (- unit-cost actual-weighted-average-price) quantity)
+        (+ (:loss acc) (h/calculate-profit acc operation))
+        ))))
 
 (s/defn calculate-operation-value [operation :- m/Operation]
-	(* (:quantity operation) (:unit-cost operation)))
+  (* (:quantity operation) (:unit-cost operation)))
 
 (s/defn operation-value-exceeds-tax-limit? [operation]
-	(< 20000.00 (calculate-operation-value operation)))
+  (< operation-limit (calculate-operation-value operation)))
 
 (s/defn apply-tax [profit operation]
-	(if (operation-value-exceeds-tax-limit? operation)
-		{:tax (* profit 0.2)}
-		(return-empty-tax)))
+  (if (operation-value-exceeds-tax-limit? operation)
+    {:tax (* profit tax)}
+    empty-tax))
 
-(s/defn hass-loss? [loss] (> 0 loss))
-(s/defn hass-profit? [profit-with-loss-deducted] (< 0 profit-with-loss-deducted))
+(s/defn has-loss? [loss] (> 0 loss))
+(s/defn has-profit? [profit-with-loss-deducted] (< 0 profit-with-loss-deducted))
 
 (s/defn calculate-tax-deducting-loss-if-it-exists
-	[acc :- m/Accumulator operation :- m/Operation]
-	(let [profit (h/calculate-profit acc operation)
-				loss   (:loss acc)]
-		(if (hass-loss? loss)
-			(let [profit-with-loss-deducted (+ profit loss)]
-				(if (hass-profit? profit-with-loss-deducted)
-					 (apply-tax profit-with-loss-deducted operation)
-					(return-empty-tax)
-					)
-				)
-			(apply-tax profit operation)
-			)))
+  [acc :- m/Accumulator operation :- m/Operation]
+  (let [profit (h/calculate-profit acc operation)
+        {loss :loss} acc]
+    (if (has-loss? loss)
+      (let [profit-with-loss-deducted (+ profit loss)]
+        (if (has-profit? profit-with-loss-deducted)
+          (apply-tax profit-with-loss-deducted operation)
+          empty-tax
+          )
+        )
+      (apply-tax profit operation)
+      )))
 
 (s/defn calculate-tax-if-needed :- m/Tax
-	[acc :- m/Accumulator operation :- m/Operation]
-	(if (h/is-buy-operation? operation)
-		(return-empty-tax)
-		(let [profit (h/calculate-profit acc operation)]
-			(if (hass-profit? profit)
-				(calculate-tax-deducting-loss-if-it-exists acc operation)
-				(return-empty-tax)))))
+  [acc :- m/Accumulator operation :- m/Operation]
+  (if (h/is-buy-operation? operation)
+    empty-tax
+    (let [profit (h/calculate-profit acc operation)]
+      (if (has-profit? profit)
+        (calculate-tax-deducting-loss-if-it-exists acc operation)
+        empty-tax))))
+
+(s/defn havent-weighted-average-price? [avg] (= avg 0))
 
 (s/defn calculate-weighted-average-price
-	[acc :- m/Accumulator operation :- m/Operation]
-	(if (h/is-buy-operation? operation)
-		(if (= 0 (:weighted-average-price acc))
-			(:unit-cost operation)
-			(let [actual-stocks-quantity        (:quantity acc)
-						actual-weighted-average-price (:weighted-average-price acc)
-						new-quantity                  (:quantity operation)
-						unit-cost                     (:unit-cost operation)
-						]
-				(/
-					(+
-						(* actual-stocks-quantity actual-weighted-average-price)
-						(* new-quantity unit-cost))
-					(+ actual-stocks-quantity new-quantity))))
-		(:weighted-average-price acc)
-		)
-	)
+  [{actual-quantity        :quantity
+    weighted-average-price :weighted-average-price} :- m/Accumulator
+   {unit-cost :unit-cost quantity :quantity :as operation} :- m/Operation]
+  (if (h/is-buy-operation? operation)
+    (if (havent-weighted-average-price? weighted-average-price)
+      unit-cost
+       (-> (* actual-quantity weighted-average-price)
+          (+ (* quantity unit-cost))
+          (/ (+ actual-quantity quantity)))
+      )
+    weighted-average-price))
--- test/capital_gains/logic/calculate_test.clj
+++ test/capital_gains/logic/calculate_test.clj
@@ -1,205 +1,195 @@
 (ns capital-gains.logic.calculate-test
-	(:require [capital-gains.fixtures :as fixtures]
-						[clojure.test :refer [deftest testing is]]
-						[capital-gains.logic.calculate :as calculate]))
+  (:require [capital-gains.fixtures :as fixtures]
+            [clojure.test :refer [deftest testing is]]
+            [capital-gains.logic.calculate :as calculate]))
 
-(deftest have-loss-test
-	(testing "have-loss?"
+(deftest operation-has-loss-test
+  (testing "operation-has-loss?"
 
-		(is (true? (calculate/have-loss? 1 2)))
+    (is (true? (calculate/operation-has-loss? 1 2)))
 
-		(is (false? (calculate/have-loss? 3 2)))
+    (is (false? (calculate/operation-has-loss? 3 2)))
 
-		(is (true? (calculate/have-loss? 0 2)))
+    (is (true? (calculate/operation-has-loss? 0 2)))
 
-		(is (false? (calculate/have-loss? 1 0)))
+    (is (false? (calculate/operation-has-loss? 1 0)))
 
-		(is (false? (calculate/have-loss? 0 0)))
-		))
+    (is (false? (calculate/operation-has-loss? 0 0)))
+    ))
 
 (deftest calculate-loss-test
-	(testing "buy operation" (is (=
-																 (calculate/calculate-loss
-																	 (fixtures/create-accumulator 30 0 [] 0)
-																	 (fixtures/create-operation "buy" 10 10))
-																 0)))
+  (testing "buy operation" (is (=
+                                (calculate/calculate-loss
+                                 (fixtures/create-accumulator 30 0 [] 0)
+                                 (fixtures/create-operation "buy" 10 10))
+                                0)))
 
-	(testing "sell with loss"
-		(is (=
-					(calculate/calculate-loss
-						(fixtures/create-accumulator 100 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					-900.0)))
+  (testing "sell with loss"
+    (is (=
+         (calculate/calculate-loss
+          (fixtures/create-accumulator 100 10 [] 0)
+          (fixtures/create-operation "sell" 10 10))
+         -900.0)))
 
-	(testing "sell with profit"
-		(is (=
-					(calculate/calculate-loss
-						(fixtures/create-accumulator 1 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					90)))
-
-
-	(testing "sell with same weighted-average-price"
-		(is (=
-					(calculate/calculate-loss
-						(fixtures/create-accumulator 10 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					0)))
+  (testing "sell with profit"
+    (is (=
+         (calculate/calculate-loss
+          (fixtures/create-accumulator 1 10 [] 0)
+          (fixtures/create-operation "sell" 10 10))
+         90)))
 
 
-	(testing "sell with loss and having loss on accumulator"
-		(is (=
-					(calculate/calculate-loss
-						(fixtures/create-accumulator 100 10 [] -900)
-						(fixtures/create-operation "sell" 10 10))
-					-900.0)))
+  (testing "sell with same weighted-average-price"
+    (is (=
+         (calculate/calculate-loss
+          (fixtures/create-accumulator 10 10 [] 0)
+          (fixtures/create-operation "sell" 10 10))
+         0)))
 
-	)
 
-(deftest return-empty-tax-test
-	(is (= (calculate/return-empty-tax) {:tax 0.00}))
-	)
+  (testing "sell with loss and having loss on accumulator"
+    (is (=
+         (calculate/calculate-loss
+          (fixtures/create-accumulator 100 10 [] -900)
+          (fixtures/create-operation "sell" 10 10))
+         -900.0)))
+
+  )
 
 (deftest calc-operation-value-test
-	(is (= (calculate/calculate-operation-value (fixtures/create-operation "buy" 10 10))
-				100))
+  (is (= (calculate/calculate-operation-value (fixtures/create-operation "buy" 10 10))
+         100))
 
-	(is (= (calculate/calculate-operation-value (fixtures/create-operation "buy" 0 0))
-				0))
+  (is (= (calculate/calculate-operation-value (fixtures/create-operation "buy" 0 0))
+         0))
 
-	(is (= (calculate/calculate-operation-value (fixtures/create-operation "buy" -10 100))
-				-1000))
+  (is (= (calculate/calculate-operation-value (fixtures/create-operation "buy" -10 100))
+         -1000))
 
-	)
+  )
 
 (deftest operation-value-exceeds-tax-limit?-test
-	(testing "operation value exceeds limit"
-		(is (= (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 20000.0 10))
-				true)))
-	(testing "operation value exceeds limit by 0.1"
-		(is (= (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 20000.1 1))
-					true)))
+  (testing "operation value exceeds limit"
+    (is (true? (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 20000.0 10)))))
 
-	(testing "operation value is the same of the limit"
-		(is (= (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 20000.0 1))
-					false)))
 
-	(testing "operation value is under the limit"
-		(is (= (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 2 1))
-					false)))
+  (testing "operation value exceeds limit by 0.1"
+    (is (true? (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 20000.1 1)))))
 
-	(testing "operation value is zero"
-		(is (= (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 2 0))
-					false)))
-	)
+  (testing "operation value is the same of the limit"
+    (is (false? (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 20000.0 1)))))
+
+  (testing "operation value is under the limit"
+    (is (false? (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 2 1)))))
+
+  (testing "operation value is zero"
+    (is (false? (calculate/operation-value-exceeds-tax-limit? (fixtures/create-operation "sell" 2 0))))))
 
 (deftest apply-tax-test
-	(testing "apply tax"
-		(is (= (calculate/apply-tax 100 (fixtures/create-operation "sell" 20000.0 10))
-					{:tax 20.0})))
+  (testing "apply tax"
+    (is (= (calculate/apply-tax 100 (fixtures/create-operation "sell" 20000.0 10))
+           {:tax 20.0})))
 
-	(testing "apply tax with profit under limit"
-		(is (= (calculate/apply-tax 100 (fixtures/create-operation "sell" 20000.0 1))
-					{:tax 0.0})))
+  (testing "apply tax with profit under limit"
+    (is (= (calculate/apply-tax 100 (fixtures/create-operation "sell" 20000.0 1))
+           {:tax 0.0})))
 
-	(testing "apply tax with profit under limit"
-		(is (= (calculate/apply-tax 100 (fixtures/create-operation "sell" 2 1))
-					{:tax 0.0})))
+  (testing "apply tax with profit under limit"
+    (is (= (calculate/apply-tax 100 (fixtures/create-operation "sell" 2 1))
+           {:tax 0.0})))  )
 
-	)
+(deftest has-loss?-test
+  (testing "has loss"
+    (is (true? (calculate/has-loss? -1))))
 
-(deftest hass-loss?-test
-	(testing "has loss"
-		(is (= (calculate/hass-loss? -1) true)))
+  (testing "has no loss"
+    (is (false? (calculate/has-loss? 0))))
 
-	(testing "has no loss"
-		(is (= (calculate/hass-loss? 0) false)))
+  (testing "has no loss"
+    (is (false? (calculate/has-loss? 1))))
+  )
 
-	(testing "has no loss"
-		(is (= (calculate/hass-loss? 1) false)))
-	)
+(deftest has-profit?-test
+  (testing "has profit"
+    (is (true? (calculate/has-profit? 1))))
 
-(deftest hass-profit?-test
-	(testing "has profit"
-		(is (= (calculate/hass-profit? 1) true)))
+  (testing "has no profit"
+    (is (false? (calculate/has-profit? 0))))
 
-	(testing "has no profit"
-		(is (= (calculate/hass-profit? 0) false)))
-
-	(testing "has no profit"
-		(is (= (calculate/hass-profit? -1) false)))
-	)
+  (testing "has no profit"
+    (is (false? (calculate/has-profit? -1))))
+  )
 
 (deftest calculate-tax-deducting-loss-if-it-exists-test
-	(testing "has loss"
-		(is (= (calculate/calculate-tax-deducting-loss-if-it-exists
-						(fixtures/create-accumulator 100 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					{:tax 0.0})))
+  (testing "has loss"
+    (is (= (calculate/calculate-tax-deducting-loss-if-it-exists
+            (fixtures/create-accumulator 100 10 [] 0)
+            (fixtures/create-operation "sell" 10 10))
+           {:tax 0.0})))
 
 
-	(testing "has profit"
-		(is (= (calculate/calculate-tax-deducting-loss-if-it-exists
-						(fixtures/create-accumulator 1000 10000 [] 0)
-						(fixtures/create-operation "sell" 5000 1000))
-					{:tax 800000.0})))
+  (testing "has profit"
+    (is (= (calculate/calculate-tax-deducting-loss-if-it-exists
+            (fixtures/create-accumulator 1000 10000 [] 0)
+            (fixtures/create-operation "sell" 5000 1000))
+           {:tax 800000.0})))
 
-	(testing "has profit but operation is under 20000"
-		(is (= (calculate/calculate-tax-deducting-loss-if-it-exists
-						 (fixtures/create-accumulator 10 10000 [] 0)
-						 (fixtures/create-operation "sell" 50 10))
-					{:tax 0.0})))
+  (testing "has profit but operation is under 20000"
+    (is (= (calculate/calculate-tax-deducting-loss-if-it-exists
+            (fixtures/create-accumulator 10 10000 [] 0)
+            (fixtures/create-operation "sell" 50 10))
+           {:tax 0.0})))
 
-	)
+  )
 
 (deftest calculate-tax-if-needed-test
-	(testing "buy operation"
-		(is (= (calculate/calculate-tax-if-needed
-						(fixtures/create-accumulator 100 10 [] 0)
-						(fixtures/create-operation "buy" 10 10))
-					{:tax 0.0})))
+  (testing "buy operation"
+    (is (= (calculate/calculate-tax-if-needed
+            (fixtures/create-accumulator 100 10 [] 0)
+            (fixtures/create-operation "buy" 10 10))
+           {:tax 0.0})))
 
-	(testing "sell operation with loss"
-		(is (= (calculate/calculate-tax-if-needed
-						(fixtures/create-accumulator 100 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					{:tax 0.0})))
+  (testing "sell operation with loss"
+    (is (= (calculate/calculate-tax-if-needed
+            (fixtures/create-accumulator 100 10 [] 0)
+            (fixtures/create-operation "sell" 10 10))
+           {:tax 0.0})))
 
-	(testing "sell operation with profit but operation under 20000"
-		(is (= (calculate/calculate-tax-if-needed
-						(fixtures/create-accumulator 100 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					{:tax 0.0})))
+  (testing "sell operation with profit but operation under 20000"
+    (is (= (calculate/calculate-tax-if-needed
+            (fixtures/create-accumulator 100 10 [] 0)
+            (fixtures/create-operation "sell" 10 10))
+           {:tax 0.0})))
 
-	(testing "sell operation with profit and operation over 20000"
-		(is (= (calculate/calculate-tax-if-needed
-						(fixtures/create-accumulator 100 10 [] 0)
-						(fixtures/create-operation "sell" 20000 10))
-					{:tax 39800.0})))
-	)
+  (testing "sell operation with profit and operation over 20000"
+    (is (= (calculate/calculate-tax-if-needed
+            (fixtures/create-accumulator 100 10 [] 0)
+            (fixtures/create-operation "sell" 20000 10))
+           {:tax 39800.0})))
+  )
 
 (deftest calculate-weighted-average-price-test
-	(testing "sell operation"
-		(is (= (calculate/calculate-weighted-average-price
-						(fixtures/create-accumulator 100.0 10 [] 0)
-						(fixtures/create-operation "sell" 10 10))
-					100.0)))
+  (testing "sell operation"
+    (is (= (calculate/calculate-weighted-average-price
+            (fixtures/create-accumulator 100.0 10 [] 0)
+            (fixtures/create-operation "sell" 10 10))
+           100.0)))
 
-	(testing "buy operation when unit-cost is the same of the weighted-average-price"
-		(is (= (calculate/calculate-weighted-average-price
-						(fixtures/create-accumulator 100.0 10 [] 0)
-						(fixtures/create-operation "buy" 100.0 10))
-					100.0)))
+  (testing "buy operation when unit-cost is the same of the weighted-average-price"
+    (is (= (calculate/calculate-weighted-average-price
+            (fixtures/create-accumulator 100.0 10 [] 0)
+            (fixtures/create-operation "buy" 100.0 10))
+           100.0)))
 
-	(testing "buy operation when unit-cost is under the weighted-average-price"
-		(is (= (calculate/calculate-weighted-average-price
-						(fixtures/create-accumulator 100.0 10 [] 0)
-						(fixtures/create-operation "buy" 50.0 10))
-					75.0)))
+  (testing "buy operation when unit-cost is under the weighted-average-price"
+    (is (= (calculate/calculate-weighted-average-price
+            (fixtures/create-accumulator 100.0 10 [] 0)
+            (fixtures/create-operation "buy" 50.0 10))
+           75.0)))
 
-	(testing "buy operation when unit-cost is over the weighted-average-price"
-		(is (= (calculate/calculate-weighted-average-price
-						(fixtures/create-accumulator 100.0 10 [] 0)
-						(fixtures/create-operation "buy" 150.0 10))
-					125.0)))
-	)
+  (testing "buy operation when unit-cost is over the weighted-average-price"
+    (is (= (calculate/calculate-weighted-average-price
+            (fixtures/create-accumulator 100.0 10 [] 0)
+            (fixtures/create-operation "buy" 150.0 10))
+           125.0)))
+  )
